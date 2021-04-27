a3b2e82bbc8834e27f4ef65fe332f4293c49d9de
T-Wainwright <tom.wainwright@bristol.ac.uk>
2021-04-27 13:39:57 +0100

    Added static model model to solver

--- src/UoB_coupling.py
+++ src/UoB_coupling.py
@@ -23,96 +23,160 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-Generic Aero-structural coupling scheme for zcfd
-
-Tom Wainwright
-University of Bristol
-2020
 """
 import numpy as np
-import os
-import sys
 from zcfd import MPI
 from zcfd.solvers.utils.RuntimeLoader import create_abaqus_fsi
 from zcfd.utils import config
-import py_rbf
+from zcfd.utils.coupling import py_rbf
+import sys
+import matlab.engine
+
+
+# import libsimulia_cse as simulia_cse
 
 
 def post_init(self):
-    # Create aero-structural link
-    self.abaqus_fsi = create_abaqus_fsi(self.solverlib, 'case_name', 'problem_name')
+    self.abaqus_fsi = create_abaqus_fsi(self.solverlib, self.parameters['case name'], self.parameters['problem name'], self.parameters)
 
-    # Get face information 
-    self.num_faces = self.abaqus_fsi.init(self.mesh[0])
-    self.p = self.abaqus_fsi.get_pressures(self.solver_data[0], self.mesh[0])
-    self.nodes = self.abaqus_fsi.get_fsi_nodes(self.mesh[0])  # x, y, z of nodes on fsi surface
-    self.n_a = len(self.nodes) / 3                     # Number of unique nodes
-    self.nodes = np.reshape(self.nodes, [self.n_a, 3])
-    self.face_nodes = self.abaqus_fsi.get_fsi_face_nodes()  # face nodes ALL QUADS CURRENTLY
+    num_faces = self.abaqus_fsi.init(self.mesh[0])
+
+    p = self.abaqus_fsi.get_pressures(self.solver_data[0], self.mesh[0])
+    nodes = self.abaqus_fsi.get_fsi_nodes(self.mesh[0])  # x, y, z of nodes on fsi surface
+    face_nodes = self.abaqus_fsi.get_fsi_face_nodes()  # face nodes ALL QUADS CURRENTLY
+    self.num_nodes = int(len(nodes) / 3)
+
     self.rank = MPI.COMM_WORLD.Get_rank()
+
+    # Rank 0 stuff
     if self.rank == 0:
         self.node_labels = self.abaqus_fsi.get_fsi_node_labels()
-    self.pressure_force = np.zeros((self.num_faces, 3))
-    self.face_dictionary = process_face(self)
+        # Aero-preprossessing
+        self.UoB_coupling = py_rbf.UoB_coupling()
+        self.UoB_coupling.aero_nodes = np.reshape(nodes, [self.num_nodes, 3])
+        self.UoB_coupling.n_a = self.num_nodes
+        self.UoB_coupling.n_f = num_faces
+        self.UoB_coupling.face_nodes = face_nodes
+        self.UoB_coupling.node_labels = self.node_labels
+        self.UoB_coupling.process_face()
 
+        # load structural nodes
+        self.UoB_coupling.load_struct()
+        # self.UoB_coupling.load_modes()
 
-    # read structural nodes - placeholder function for now
-    loadbeamstick(self)
-    # write_struc_tec(self,'struct.plt')
-
-    write_tec(self,'aero.plt')
-    # create coupling matrices- here using two full RBF matrices
-    self.H_u = py_rbf.generate_transfer_matrix(self.aero_nodes, self.struct_nodes, self.parameters['abaqus fsi']['alpha'], polynomial=True)
-    self.H_p = py_rbf.generate_transfer_matrix(get_face_centres(self), self.struct_nodes, self.parameters['abaqus fsi']['alpha'], polynomial=True)
-
-    # Initiate deformation arrays
-    self.U_s = np.zeros((self.n_s, 3))
-    self.U_a = np.zeros((self.n_a, 3))
-
-    self.F_s = np.zeros((self.n_s, 3))
-    self.F_a = np.zeros((self.n_a, 3))
-
-    # Write initial geometry files
-    write_tec(self, 'Original_Surf.plt')
-    write_struc_tec(self, 'Original_Beam.plt')
+        self.UoB_coupling.generate_displacement_transfer_matrix(self.parameters['abaqus fsi']['alpha'], polynomial=False)
+        self.UoB_coupling.generate_pressure_transfer_matrix(self.parameters['abaqus fsi']['alpha'], polynomial=False)
 
     MPI.COMM_WORLD.Barrier()
 
-    # zCFD preprocessing
-    self.abaqus_fsi.init_rbf()
+    # RBF pre-processing
+    self.abaqus_fsi.init_morphing(self.mesh[0])
+
+    print('Finished setup')
 
 
 def start_real_time_cycle(self):
-    # zero displacement arrays
-    self.U_a = np.zeros((self.n_a, 3))
-    self.U_s = np.zeros((self.n_s, 3))
-    # Read structural displaments
-    self.U_s = loadDisplacements(self)
-    
-    self.U_a = py_rbf.interp_displacements(self.U_s, self.H_u)
-
-    self.nodes[:,0] = self.nodes[:,0] + self.U_a[:, 0]
-    self.nodes[:,1] = self.nodes[:,1] + self.U_a[:, 1]
-    self.nodes[:,2] = self.nodes[:,2] + self.U_a[:, 2]
-
-    write_tec(self, 'deformed_aero.plt')
-
-    # Deform volume mesh
+    U_a = [0.0 * ii for ii in range(self.num_nodes * 3)]
     dt = self.real_time_step
 
-    self.U_a = MPI.COMM_WORLD.bcast(self.U_a, root=0)
+    p = self.abaqus_fsi.get_pressures(self.solver_data[0], self.mesh[0])
+
+    if self.rank == 0:
+        for ii in range(len(p)):
+            p[ii] -= self.parameters['ic_2']['pressure']
+            # print("ii {} pressure {}".format(ii, p[ii]), flush=True)
+
+        F_a = self.UoB_coupling.calculate_pressure_force(p)
+        F_s = self.UoB_coupling.interp_forces(F_a)
+
+        U_s = self.UoB_coupling.call_ATOM(F_s)
+        U_s = U_s[:, s0:3]
+        U_a = self.UoB_coupling.interp_displacements(U_s)
+        U_a = list(U_a.flatten(order='C'))
+
+        max_disp = 0.0
+
+        for ii in range(self.num_nodes):
+            idx = ii * 3
+            disp = np.sqrt(U_a[idx] * U_a[idx] + U_a[idx + 1] * U_a[idx + 1] + U_a[idx + 2] * U_a[idx + 2])
+            max_disp = max(max_disp, disp)
+
+        print("max displacement {}".format(max_disp), flush=True)
+        # delta  = -40.0 * float(self.real_time_step) * float(self.solve_cycle)
+        # for ii in range(self.num_nodes):
+        #     u[ii*3+2] = delta
+
+        # print("deformation dist {}".format(delta), flush=True)
+
+    # sys.exit()
+    # if self.rank == 0:
+    #     lockstep = 1
+    #     controls = 0
+    #     current_time = self.real_time_cycle * self.real_time_step if self.local_timestepping else (self.solve_cycle - 1) * self.real_time_step
+    #     target_time = (self.real_time_cycle + 1) * self.real_time_step if self.local_timestepping else (self.solve_cycle) * self.real_time_step
+
+    #     time_data = self.cse.get_target_time(current_time, self.real_time_step, target_time, self.real_time_step, lockstep, controls)
+    #     target_time = time_data[0]
+    #     dt = time_data[1]
+    #     lockstep = time_data[2]
+    #     controls = time_data[3]
+
+    #     self.cse.notify_start()
+
+    #     self.cse.get_field("displacement", "zCFD Mesh", "NULL", self.num_nodes, self.node_labels, target_time, u)
+
+    U_a = MPI.COMM_WORLD.bcast(U_a, root=0)
     dt = MPI.COMM_WORLD.bcast(dt, root=0)
     # Perform RBF and mesh updates
-    self.abaqus_fsi.deform_mesh(self.mesh[0], list(self.U_a), dt)
-    print("Finished start real time cycle")
-    sys.exit()
+    self.abaqus_fsi.deform_mesh(self.mesh[0], U_a, dt, False)
 
 
 def post_advance(self):
-    p = self.abaqus_fsi.get_pressures(self.solver_data[0], self.mesh[0])
-    if self.rank == 0:
-        t = (self.real_time_cycle + 1) * self.real_time_step if self.local_timestepping else (self.solve_cycle) * self.real_time_step
+    if self.solve_cycle % 50 == 0 and self.solve_cycle > 0 and self.real_time_step > 100.0:
+        U_a = [0.0 * ii for ii in range(self.num_nodes * 3)]
+        dt = self.real_time_step
+
+        p = self.abaqus_fsi.get_pressures(self.solver_data[0], self.mesh[0])
+
+        if self.rank == 0:
+            for ii in range(len(p)):
+                p[ii] -= self.parameters['ic_2']['pressure']
+
+            # Calculate deformed face normals
+            self.UoB_coupling.aero_nodes = np.reshape(self.abaqus_fsi.get_fsi_nodes(self.mesh[0]), [self.num_nodes, 3])
+            self.UoB_coupling.process_face()
+
+            F_a = self.UoB_coupling.calculate_pressure_force(p)
+            print("Aero force summation: {} \t {} \t {}\n".format(np.sum(F_a[:, 0]), np.sum(F_a[:, 1]), np.sum(F_a[:, 2])), flush=True)
+            F_s = self.UoB_coupling.interp_forces(F_a)
+            print("Struct force summation: {} \t {} \t {}\n".format(np.sum(F_s[:, 0]), np.sum(F_s[:, 1]), np.sum(F_s[:, 2])), flush=True)
+
+            U_s = self.UoB_coupling.call_ATOM(F_s)
+            U_s = U_s[:, 0:3]
+            U_a = self.UoB_coupling.interp_displacements(U_s)
+            U_a = list(U_a.flatten(order='C'))
+
+            max_disp = 0.0
+            for ii in range(self.num_nodes):
+                idx = ii * 3
+                disp = np.sqrt(U_a[idx] * U_a[idx] + U_a[idx + 1] * U_a[idx + 1] + U_a[idx + 2] * U_a[idx + 2])
+                max_disp = max(max_disp, disp)
+
+            print("max displacement {}".format(max_disp), flush=True)
+
+            self.UoB_coupling.write_deformed_struct(U_s)
+
+        U_a = MPI.COMM_WORLD.bcast(U_a, root=0)
+        dt = MPI.COMM_WORLD.bcast(dt, root=0)
+        # Perform RBF and mesh updates
+        self.abaqus_fsi.deform_mesh(self.mesh[0], U_a, dt, False)
+
+    # p = self.abaqus_fsi.get_pressures(self.solver_data[0], self.mesh[0])
+    # if self.rank == 0:
+    #     self.cse.set_pressure(p)
+    #     t = (self.real_time_cycle + 1) * self.real_time_step if self.local_timestepping else (self.solve_cycle) * self.real_time_step
+
+#        self.cse.notify_end(t)
 
 
 def post_output(self):
@@ -120,145 +184,7 @@ def post_output(self):
     pass
 
 
-# Custom functions -----------------------------------------
-# ----------------------------------------------------------
-# ----------------------------------------------------------
-
-# Data processing
-
-def process_face(self):
-    # Process face information into more easily distinguishable dictionary format
-    face = {}
-    for f in range(self.num_faces):
-        face[f] = {}
-        for i in range(4):
-            index = f * 4 + i
-            face[f][i] = {}
-            face[f][i]['id'] = self.face_nodes[index]
-            face[f][i]['index_id'] = self.node_labels.index(self.face_nodes[index]) + 1
-            face[f][i]['coord'] = self.nodes[self.node_labels.index(self.face_nodes[index])]
-            face[f]['pressure'] = self.p[f]
-        face[f]['norm'] = -np.cross([np.array(face[f][0]['coord'])-np.array(face[f][1]['coord'])],[np.array(face[f][0]['coord'])-np.array(face[f][3]['coord'])])[0]
-        face[f]['unit_norm'] = face[f]['norm']/np.linalg.norm(face[f]['norm'])
-        face[f]['px'] = face[f]['unit_norm'][0] * face[f]['pressure']
-        face[f]['py'] = face[f]['unit_norm'][1] * face[f]['pressure']
-        face[f]['pz'] = face[f]['unit_norm'][2] * face[f]['pressure']
-        face[f]['centre'] = (face[f][0]['coord'] + face[f][1]['coord'] + face[f][2]['coord'] + face[f][3]['coord']) /4
-        self.pressure_force[f,:] = [face[f]['px'],face[f]['py'],face[f]['pz']]
-
-    face['num_faces'] = self.num_faces
-    face['n_a'] = self.n_a
-
-    return face
-
-
-def calculate_pressure_force(self, p):
-    for f in range(self.num_faces):
-        px = self.face[f]['unit_norm'][0] * p[f]
-        py = self.face[f]['unit_norm'][1] * p[f]
-        pz = self.face[f]['unit_norm'][2] * p[f]
-
-        self.pressure_force[f,:] = [px, py, pz]
-
-    return
-
-
-def get_face_centres(self):
-    face_centre = np.zeros((self.num_faces, 3))
-    for i in range(self.num_faces):
-        face_centre[i,:] = self.face_dictionary[i]['centre']
-    return face_centre
-
-
-# Data writing
-
-def write_tec(self, fname):
-    f = open(fname, "w")
-    f.write("TITLE = \"Surface plot\"\n")
-    f.write("VARIABLES = \"X\", \"Y\", \"Z\", \"normx\", \"normy\", \"normz\", \"px\", \"py\", \"pz\"\n")
-    f.write("ZONE T=\"PURE-QUADS\", NODES={}, ELEMENTS={}, DATAPACKING=BLOCK, VARLOCATION=([4,5,6,7,8,9]=CELLCENTERED), ZONETYPE=FEQUADRILATERAL\n".format(self.n_a,self.num_faces))
-    for i in range(self.n_a):
-        f.write("{}\n".format(self.nodes[i, 0]))
-    for i in range(self.n_a):
-        f.write("{}\n".format(self.nodes[i, 1]))
-    for i in range(self.n_a):
-        f.write("{}\n".format(self.nodes[i, 2]))
-    for i in range(self.num_faces):
-        f.write("{}\n".format(self.face_dictionary[i]['unit_norm'][0]))
-    for i in range(self.num_faces):
-        f.write("{}\n".format(self.face_dictionary[i]['unit_norm'][1]))
-    for i in range(self.num_faces):
-        f.write("{}\n".format(self.face_dictionary[i]['unit_norm'][2]))
-    for i in range(self.num_faces):
-        f.write("{}\n".format(self.pressure_force[i, 0]))
-    for i in range(self.num_faces):
-        f.write("{}\n".format(self.pressure_force[i, 1]))
-    for i in range(self.num_faces):
-        f.write("{}\n".format(self.pressure_force[i, 2]))
-    for i in range(self.num_faces):
-        f.write("{} {} {} {}\n".format(self.face_dictionary[i][0]['index_id'], self.face_dictionary[i][1]['index_id'], self.face_dictionary[i][2]['index_id'], self.face_dictionary[i][3]['index_id']))
-    
-    f.close()
-
-
-def write_struc_tec(self, fname):
-
-    # Write beam nodes to tecplot file
-    f = open(fname, "w")
-    f.write("TITLE = \"Beamstick model\"\n")
-    f.write("VARIABLES = \"X\", \"Y\", \"Z\" \"px\", \"py\", \"pz\"\n")
-    f.write("ZONE I={}, J=1, K=1\n".format(self.n_s))
-    for i in range(self.n_s):
-        f.write("{} {} {} {} {} {}\n".format(self.struct_nodes[i][0], self.struct_nodes[i][1], self.struct_nodes[i][2], self.F_s[i, 0], self.F_s[i, 1], self.F_s[i, 2]))
-    f.close()
-
-
-def write_structural_forces(self):
-    f = open("pforce_struc.dat", "w")
-    f.write("{}\n".format(self.num_faces))
-    for i in range(self.num_faces):
-        f.write("{} {} {}\n".format(self.face_dictionary[i]['px'], self.face_dictionary[i]['py'], self.face_dictionary[i]['pz']))
-    f.close()
-
-
-def write_surf(self):
-    f = open("aero_nodes.xyz", "w")
-    f.write("{}\n".format(self.n_a))
-    for i in range(self.n_a):
-        f.write("{} {} {}\n".format(self.nodes[i, 0], self.nodes[i, 1], self.nodes[i, 2]))
-    f.close()
-
-
-# Loading data
-
-
-def loadbeamstick(self):
-    self.struct_nodes = np.loadtxt('beamstick.dat', skiprows=4)
-    self.n_s = len(self.struct_nodes[:, 0])
-    beam_forward = self.struct_nodes
-    beam_aft = self.struct_nodes
-    for i in range(self.n_s):
-        beam_forward[i, 0] = beam_forward[i, 2] + 1
-        beam_aft[i, 0] = beam_aft[i, 2] - 1
-    
-    self.struct_nodes = np.append(self.struct_nodes, beam_forward, axis=0)
-    self.struct_nodes = np.append(self.struct_nodes, beam_aft, axis=0)
-    print(self.struct_nodes.shape)
-    self.n_s = self.n_s * 3
-    return
-    
-def loadDisplacements(self):
-    u = np.loadtxt('deformed.dat',skiprows=4)
-    u1 = u
-    u = np.append(u, u1, axis=0)
-    u = np.append(u, u1, axis=0)
-    return u
-
-
-
-    
-
-uob_fsi_coupling = {'post init hook': post_init,
+abaqus_fsi_coupling = {'post init hook': post_init,
                        'start real time cycle hook': start_real_time_cycle,
                        'post advance hook': post_advance,
                        'post output hook': post_output}
--- src/py_rbf.py
+++ src/py_rbf.py
@@ -1,25 +1,28 @@
 import numpy as np
+import scipy.io
+
 
 # Functions
 
 class UoB_coupling():
 
     def __init__(self):
-        pass
+        self.ModalStruct = scipy.io.loadmat('ModalModel.mat')['ModalStruct']
+        self.BladeFE = scipy.io.loadmat('Blade_FE.mat')
 
     def load_struct(self, fname):
-        self.struct_nodes = np.loadtxt(fname, skiprows=1)
-        self.n_s = len(self.struct_nodes[:, 0])
+        self.struct_nodes = self.BladeFE['BeamAxis_123'][0][0]
+        self.n_s = self.struct_nodes.shape[0]
 
     def load_modes(self):
-        self.Kmodal_inv = np.loadtxt('Kmodal_inv.dat')
-        self.Eigvec = np.loadtxt('Eigvec.dat')
-        self.nEigval = self.Eigvec.shape[1]
+        self.nEigval = self.ModalStruct['nEigval'][0][0][0][0]
+        self.Eigvec = self.ModalStruct['Eigvec'][0][0]
+        self.KmodalInv = self.ModalStruct['KmodalInv'][0][0]
 
     def process_face(self):
         # Process face information into more easily distinguishable dictionary format
         self.face = {}
-        self.aero_centres = np.zeros((self.n_f,3))
+        self.aero_centres = np.zeros((self.n_f, 3))
         for f in range(self.n_f):
             self.face[f] = {}
             for i in range(4):
@@ -39,40 +42,40 @@ class UoB_coupling():
     def generate_pressure_transfer_matrix(self, r0, polynomial):
         self.H_p = generate_transfer_matrix(self.aero_centres, self.struct_nodes, r0, polynomial)
 
-    def calculate_pressure_force(self,p):
-        pressure_force = np.zeros((self.n_f,3))
+    def calculate_pressure_force(self, p):
+        pressure_force = np.zeros((self.n_f, 3))
         for f in range(self.n_f):
             pressure_force[f, :] = p[f] * self.face[f]['norm']
         # print('Max aero force = {}'.format(np.max(pressure_force)))
         return pressure_force
 
-    def interp_forces(self,F_a):
+    def interp_forces(self, F_a):
         F_s = interp_forces(F_a, self.H_p)
         # print('Max structural force = {}'.format(np.max(F_s)))
         return F_s
-    
-    def interp_displacements(self,U_s):
-        U_a = interp_displacements(U_s,self.H_u)
+
+    def interp_displacements(self, U_s):
+        U_a = interp_displacements(U_s, self.H_u)
         for i in range(3):
-            self.aero_nodes[:,i] = self.aero_nodes[:,i] + U_a[:,i]
+            self.aero_nodes[:, i] = self.aero_nodes[:, i] + U_a[:, i]
 
         np.savetxt('deformed.csv', self.aero_nodes, delimiter=",")
         return U_a
 
-    def deform_struct(self,F_s):
+    def deform_struct(self, F_s):
         # Rearrange F_s to match ATOM coordinate system
         F_s_atom = np.zeros_like(F_s)
-        F_s_atom[:,0] = F_s[:,1]
-        F_s_atom[:,1] = -F_s[:,2]
-        F_s_atom[:,2] = -F_s[:,0]
+        F_s_atom[:, 0] = F_s[:, 1]
+        F_s_atom[:, 1] = -F_s[:, 2]
+        F_s_atom[:, 2] = -F_s[:, 0]
         # Reshape F_s
         F_s_atom = np.concatenate((F_s_atom, np.zeros_like(F_s_atom)), axis=1)
         F_s_atom = F_s_atom.flatten(order='F')
 
         # Convert Nodal forces to modal forces
-        FModal = np.matmul(np.transpose(self.Eigvec[:, 0:self.nEigval]), F_s_atom[6:])
+        FModal = np.matmul(np.transpose(Eigvec[:, 0:nEigval]), Fnodal[6:])
 
-        QDisp = np.matmul(self.Kmodal_inv, FModal)
+        QDisp = np.matmul(self.KmodalInv, FModal)
 
         Disp = np.zeros(self.Eigvec.shape[0])
 
@@ -88,118 +91,12 @@ class UoB_coupling():
 
         Disp_zcfd = np.zeros_like(Disp)
 
-        Disp_zcfd[:,0] = -Disp[:,2]
-        Disp_zcfd[:,1] = Disp[:,0]
-        Disp_zcfd[:,2] = -Disp[:,1]
-
-        # Adjust scale factor here
-        return(Disp_zcfd * 1.0e-0)
-
-
-class UoB_coupling():
-    def __init__(self):
-        pass
-
-    def load_struct(self, fname):
-        # Load beamstick model
-        self.struct_nodes = np.loadtxt(fname, skiprows=1)
-        self.n_s = len(self.struct_nodes[:, 0])
-
-    def load_modes(self):
-        # Load data dumped from atom- future use matlab API
-        self.Kmodal_inv = np.loadtxt('Kmodal_inv.dat')
-        self.Eigvec = np.loadtxt('Eigvec.dat')
-        self.nEigval = self.Eigvec.shape[1]
-
-    def process_face(self):
-        # Process face information into more easily distinguishable dictionary format
-        self.face = {}
-        self.aero_centres = np.zeros((self.n_f, 3))
-        for f in range(self.n_f):
-            self.face[f] = {}
-            for i in range(4):
-                index = f * 4 + i
-                self.face[f][i] = {}
-                self.face[f][i]['id'] = self.face_nodes[index]
-                self.face[f][i]['index_id'] = self.node_labels.index(self.face_nodes[index]) + 1
-                self.face[f][i]['coord'] = self.aero_nodes[self.node_labels.index(self.face_nodes[index])]
-            self.face[f]['norm'] = np.cross([np.array(self.face[f][0]['coord']) - np.array(self.face[f][1]['coord'])], [np.array(self.face[f][0]['coord']) - np.array(self.face[f][3]['coord'])])[0]
-            self.face[f]['unit_norm'] = self.face[f]['norm'] / np.linalg.norm(self.face[f]['norm'])
-            self.face[f]['centre'] = (self.face[f][0]['coord'] + self.face[f][1]['coord'] + self.face[f][2]['coord'] + self.face[f][3]['coord']) / 4
-            self.aero_centres[f] = self.face[f]['centre']
-
-    def generate_displacement_transfer_matrix(self, r0, polynomial):
-        # Calculate displacement transfer matrix using full RBF
-        self.H_u = generate_transfer_matrix(self.aero_nodes, self.struct_nodes, r0, 'c2', polynomial)
-
-    def generate_pressure_transfer_matrix(self, r0, polynomial):
-        # Calculate pressure transfer matrix using full RBF
-        self.H_p = generate_transfer_matrix(self.aero_centres, self.struct_nodes, r0, 'c2', polynomial)
-
-    def calculate_pressure_force(self, p):
-        # Calculate pressure forces acting normal the the blade surface- add datum pressure to this step in future.
-        pressure_force = np.zeros((self.n_f, 3))
-        for f in range(self.n_f):
-            pressure_force[f, :] = p[f] * self.face[f]['norm']
-        return pressure_force
-
-    def interp_forces(self, F_a):
-        # Interpolate aerodynamic forces to blade
-        F_s = interp_forces(F_a, self.H_p)
-        return F_s
-
-    def interp_displacements(self, U_s):
-        # Interpolate strucutral displacements to aerodynamic centre
-        U_a = interp_displacements(U_s, self.H_u)
-        for i in range(3):
-            self.aero_nodes[:, i] = self.aero_nodes[:, i] + U_a[:, i]
-        np.savetxt('deformed.csv', self.aero_nodes, delimiter=",")
-        return U_a
-
-    def deform_struct(self, F_s):
-        # Solve modal model from data generated from ATOM.#
-
-        # Rearrange F_s to match ATOM coordinate system - to change with new mesh.
-        # x positive spanwise
-        # y positive downwards
-        # z positive edgewise
-
-        F_s_atom = np.zeros_like(F_s)
-        F_s_atom[:, 0] = F_s[:, 1]
-        F_s_atom[:, 1] = -F_s[:, 2]
-        F_s_atom[:, 2] = -F_s[:, 0]
-        # Reshape F_s- zeros for discounting moment forces for now
-        F_s_atom = np.concatenate((F_s_atom, np.zeros_like(F_s_atom)), axis=1)
-        F_s_atom = F_s_atom.flatten(order='C')
-
-        # Convert Nodal forces to modal forces
-        FModal = np.matmul(np.transpose(self.Eigvec[:, 0:self.nEigval]), F_s_atom[6:])
-
-        QDisp = np.matmul(self.Kmodal_inv, FModal)
-
-        Disp = np.zeros(self.Eigvec.shape[0])
-
-        # Solve modal model
-        for i in range(self.nEigval):
-            Disp = np.add(Disp, (QDisp[i] * self.Eigvec[:, i]))
-
-        Disp = np.concatenate((np.zeros(6), Disp))
-
-        Disp = np.reshape(Disp, (self.n_s, 6), order='C')
-
-        # Convert the coordinate systems back - To change with new mesh.
-        # x negative edgewise (positive in flow direction)
-        # y positive spanwise
-        # z upwards
-
-        Disp_zcfd = np.zeros_like(Disp)
-
-        Disp_zcfd[:, 0] = Disp[:, 2]
-        Disp_zcfd[:, 1] = -Disp[:, 0]
+        Disp_zcfd[:, 0] = -Disp[:, 2]
+        Disp_zcfd[:, 1] = Disp[:, 0]
         Disp_zcfd[:, 2] = -Disp[:, 1]
 
         # Adjust scale factor here
-        return(Disp_zcfd * 1.0e-1)
+        return(Disp_zcfd * 1.0e-0)
 
     def write_deformed_struct(self, U_s):
         # Dump out record of deformed structure
@@ -242,7 +139,7 @@ def generate_transfer_matrix(aero_nodes, struct_nodes, r0, rbf='c2', polynomial=
         for j in range(n_s):
             rad = (np.linalg.norm((struct_nodes[i] - struct_nodes[j]))) / r0
             if rad <= 1.0:
-                M_ss[i][j] = rbf(rad) 
+                M_ss[i][j] = rbf(rad)
         if polynomial:
             P_s[1:, i] = struct_nodes[i]
 
@@ -274,6 +171,7 @@ def generate_transfer_matrix(aero_nodes, struct_nodes, r0, rbf='c2', polynomial=
         H = np.matmul(A_as, M_inv)
     return H
 
+
 def interp_displacements(U_s, H):
     # Interpolate structural displacements U_s to aerodynamic surface nodes
     U_a = np.zeros((H.shape[0], U_s.shape[1]))
@@ -282,16 +180,17 @@ def interp_displacements(U_s, H):
         U_a[:, i] = np.matmul(H, U_s[:, i])
     return U_a
 
+
 def interp_forces(F_a, H):
     # Interpolate aerodynamic forces F_a to structural nodes
     print(H.shape)
     print(F_a.shape)
     F_s = np.zeros((H.shape[1], F_a.shape[1]))
 
-
     for i in range(F_a.shape[1]):
         F_s[:, i] = np.matmul(np.transpose(H), F_a[:, i])
     return F_s
+
 
 def c0(r):
     psi = (1 - r)**2

bb80129a323b4a284a3fbd42df1c6bf139864b00
T-Wainwright <tom.wainwright@bristol.ac.uk>
2021-11-10 17:09:18 +0000

    adding control cage class

--- src/opt_utils.py
+++ src/opt_utils.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import os
 import py_rbf
 import h5py
+import yaml
 
 
 class aerofoil_points():
@@ -28,16 +29,223 @@ class aerofoil_points():
 
 
 class control_cage():
+    def __init__(self, blade, n_sections, cage_slice):
+        self.n_sections = n_sections
+        self.n_pointsPerSection = len(cage_slice[:, 0])
+        self.n_cagePoints = self.n_sections * self.n_pointsPerSection
+        self.n_passengers = (self.n_sections - 1) * self.n_pointsPerSection
+
+        # Construct cage from blade geoemetry
+
+        chord_d = blade.outer_shape['chord']
+        twist_d = blade.outer_shape['twist']
+        pitch_axis_d = blade.outer_shape['pitch_axis']
+        x_d = blade.outer_shape['reference_axis']['x']
+        y_d = blade.outer_shape['reference_axis']['y']
+        z_d = blade.outer_shape['reference_axis']['z']
+
+        thick_d = {'grid': [], 'values': []}
+
+        for section in blade.outer_shape['airfoil_position']['labels']:
+            print(section)
+            thick_d['values'].append(next(item for item in blade.airfoils if item["name"] == section)['relative_thickness'])
+
+        for grid in blade.outer_shape['airfoil_position']['grid']:
+            thick_d['grid'].append(grid)
+
+        # Controls region of blade cage is placed over
+        self.z_locations = np.linspace(0.15, 0.99, n_sections)
+
+        # Interpolate blade geometry onto sections
+
+        self.chord_i = np.interp(self.z_locations, chord_d['grid'], chord_d['values'])
+        self.twist_i = np.interp(self.z_locations, twist_d['grid'], twist_d['values'])
+        self.thick_i = np.interp(self.z_locations, thick_d['grid'], thick_d['values'])
+        self.pitch_axis_i = np.interp(self.z_locations, pitch_axis_d['grid'], pitch_axis_d['values'])
+        self.x_i = np.interp(self.z_locations, x_d['grid'], x_d['values'])
+        self.y_i = np.interp(self.z_locations, y_d['grid'], y_d['values'])
+        self.z_i = np.interp(self.z_locations, z_d['grid'], z_d['values'])
+
+        self.cage = np.zeros((self.n_sections, self.n_pointsPerSection, 3))
+        self.passenger_nodes = np.zeros((self.n_sections - 1, self.n_pointsPerSection, 3))
+        self.deformations = np.zeros_like(self.cage)
+        self.passenger_deformations = np.zeros_like(self.passenger_nodes)
+
+        for i in range(self.n_sections):
+            local_cage_x = (cage_slice[:, 0] - self.pitch_axis_i[i]) * self.chord_i[i]
+            local_cage_y = cage_slice[:, 1] * self.chord_i[i] * self.thick_i[i]
+            local_cage_z = np.ones_like(local_cage_x) * self.z_i[i]
+
+            local_cage_x_twist = local_cage_x * np.cos(self.twist_i[i]) - local_cage_y * np.sin(self.twist_i[i])
+            local_cage_y_twist = local_cage_y * np.cos(self.twist_i[i]) + local_cage_x * np.sin(self.twist_i[i])
+
+            # rotate 90 around reference axis
+            for j in range(self.n_pointsPerSection):
+                self.cage[i, j, :] = np.c_[local_cage_y_twist[j] + self.x_i[i], local_cage_x_twist[j] + self.y_i[i], local_cage_z[j] + 3]
+
+        self.setup_passenger_nodes()
+
+    def setup_passenger_nodes(self):
+        for i in range(self.n_sections - 1):
+            for j in range(self.n_pointsPerSection): 
+                self.passenger_nodes[i, j, :] = (self.cage[i, j, :] + self.cage[i + 1, j, :]) / 2
+
+    def deform_passenger_nodes(self):
+        for i in range(self.n_sections - 1):
+            for j in range(self.n_pointsPerSection): 
+                self.passenger_deformations[i, j, :] = (self.deformations[i, j, :] * np.sin(np.pi / 4) ** 2 + self.deformations[i + 1, j, :] * np.cos(np.pi / 4) ** 2)
+
+    def write_passengers(self, fname): 
+        # Write control cage file to tecplot
+        f = open(fname, 'w')
+
+        f.write('TITLE = \" Passenger Nodes\" \n')
+        f.write('VARIABLES = \"X\" \"Y\" \"Z\" \n')
+        f.write('ZONE I= {} J = {} F=point \n'.format(self.n_pointsPerSection, self.n_sections - 1))
+        for i in range(self.n_sections - 1):
+            for j in range(self.n_pointsPerSection):
+                f.write('{} \t {} \t {}\n'.format(self.passenger_nodes[i, j, 0], self.passenger_nodes[i, j, 1], self.passenger_nodes[i, j, 2]))
+
+        f.close()
+
+    def write_deformed_passengers(self, fname): 
+        # Write control cage file to tecplot
+        f = open(fname, 'w')
+
+        f.write('TITLE = \" Passenger Nodes Deformed\" \n')
+        f.write('VARIABLES = \"X\" \"Y\" \"Z\" \n')
+        f.write('ZONE I= {} J = {} F=point \n'.format(self.n_pointsPerSection, self.n_sections - 1))
+        for i in range(self.n_sections - 1):
+            for j in range(self.n_pointsPerSection):
+                f.write('{} \t {} \t {}\n'.format(self.passenger_nodes[i, j, 0] + self.passenger_deformations[i, j, 0], self.passenger_nodes[i, j, 1] + self.passenger_deformations[i, j, 1], self.passenger_nodes[i, j, 2] + self.passenger_deformations[i, j, 2]))
+
+        f.close()
+
+    def convert_section_deformations(self, deformations, section):
+        # Scale deformations for section- similar process to casting blade, but only scalings and rotations are applied- no offset.
+        local_cage_x = deformations[:, 0] * self.chord_i[section]
+        local_cage_y = deformations[:, 1] * self.chord_i[section] * self.thick_i[section]
+        local_cage_z = np.zeros_like(local_cage_x)
+
+        local_cage_x_twist = local_cage_x * np.cos(self.twist_i[section]) - local_cage_y * np.sin(self.twist_i[section])
+        local_cage_y_twist = local_cage_y * np.cos(self.twist_i[section]) + local_cage_x * np.sin(self.twist_i[section])
+
+        # rotate 90 around reference axis
+        for j in range(self.n_pointsPerSection):
+            self.deformations[section, j, :] = np.c_[local_cage_y_twist[j], local_cage_x_twist[j], local_cage_z[j]]
+        
+        self.deform_passenger_nodes()
+
+    def write_cage(self, fname): 
+        # Write control cage file to tecplot
+        f = open(fname, 'w')
+
+        f.write('TITLE = \" Control Cage\" \n')
+        f.write('VARIABLES = \"X\" \"Y\" \"Z\" \n')
+        f.write('ZONE I= {} J = {} F=point \n'.format(self.n_pointsPerSection, self.n_sections))
+        for i in range(self.n_sections):
+            for j in range(self.n_pointsPerSection):
+                f.write('{} \t {} \t {}\n'.format(self.cage[i, j, 0], self.cage[i, j, 1], self.cage[i, j, 2]))
+
+        f.close()
+
+    def write_deformed(self, fname):
+        # Write deformed control cage file to tecplot
+        f = open(fname, 'w')
+
+        f.write('TITLE = \" Deformed Control Cage\" \n')
+        f.write('VARIABLES = \"X\" \"Y\" \"Z\" \n')
+        f.write('ZONE I= {} J = {} F=point \n'.format(self.n_pointsPerSection, self.n_sections))
+        for i in range(self.n_sections):
+            for j in range(self.n_pointsPerSection):
+                f.write('{} \t {} \t {}\n'.format(self.cage[i, j, 0] + self.deformations[i, j, 0], self.cage[i, j, 1] + self.deformations[i, j, 1], self.cage[i, j, 2] + self.deformations[i, j, 2]))
+
+        f.close()
+
+    def dump_points(self):
+        # Dump out control cage points into single array- needed to hook up with rbf coupler
+        points_array = np.zeros((self.n_cagePoints + self.n_passengers, 3))
+        ii = 0
+
+        for i in range(self.n_sections):
+            for j in range(self.n_pointsPerSection):
+                points_array[ii, :] = self.cage[i, j, :]
+                ii += 1
+
+        for i in range(self.n_sections - 1):
+            for j in range(self.n_pointsPerSection):
+                points_array[ii, :] = self.passenger_nodes[i, j, :]
+                ii += 1
+
+        return points_array
+
+    def dump_deformations(self):
+        # Dump out control cage points into single array- needed to hook up with rbf coupler
+        points_array = np.zeros((self.n_cagePoints + self.n_passengers, 3))
+        ii = 0
+
+        for i in range(self.n_sections):
+            for j in range(self.n_pointsPerSection):
+                points_array[ii, :] = self.deformations[i, j, :]
+                ii += 1
+
+        for i in range(self.n_sections - 1):
+            for j in range(self.n_pointsPerSection):
+                points_array[ii, :] = self.passenger_deformations[i, j, :]
+                ii += 1
+
+        return points_array
+
+
+class Blade_geom:
+    """
+    This class renders one blade for the rotor.
+    """
+
+    def __init__(self, yaml_filename: str):
+        """
+        The constructor opens the YAML file and extracts the blade
+        and airfoil information into instance attributes.
+
+        Parameters
+        ----------
+        yaml_filename: str
+            Filename that contains the geometry for the rotor.
+        """
+        geometry = yaml.load(open(yaml_filename, "r"), yaml.FullLoader)
+        self.outer_shape = geometry["components"]["blade"]["outer_shape_bem"]
+        self.airfoils = geometry["airfoils"]
+
+
+class Blade_surf:
     def __init__(self, fname):
-        # load from cba format
-        data = np.loadtxt(fname)
-        self.n_xy = int(data[0, 0])
-        self.n_z = int(data[0, 1])
+        self.points = np.loadtxt(fname, skiprows=3)
+        self.n_p = len(self.points[:, 0])
+        self.n_i = 161
+        self.n_j = 385
 
-        self.points = data[1:, :]
+    def write_surf(self, fname):
+        f = open(fname, 'w')
 
-    def get_slice0(self):
-        return add_z(self.points[0: self.n_xy, :])
+        f.write('TITLE = \" IEA_original_surface\" \n')
+        f.write('VARIABLES = \"X\" \"Y\" \"Z\" \n')
+        f.write('ZONE I= {} J = {} F=point \n'.format(self.n_i, self.n_j))
+        for i in range(self.n_sections):
+            f.write('{} \t {} \t {}\n'.format(self.points[i, 0], self.points[i, 1], self.points[i, 2]))
+
+        f.close()
+
+    def write_deformed(self, fname, deformations):
+        f = open(fname, 'w')
+
+        f.write('TITLE = \" IEA_original_surface\" \n')
+        f.write('VARIABLES = \"X\" \"Y\" \"Z\" \n')
+        f.write('ZONE I= {} J = {} F=point \n'.format(self.n_i, self.n_j))
+        for i in range(self.n_p):
+            f.write('{} \t {} \t {}\n'.format(self.points[i, 0] + deformations[i, 0], self.points[i, 1] + deformations[i, 1], self.points[i, 2] + deformations[i, 2]))
+
+        f.close()
+
 
 
 def generate_modes(data_dir, save_loc):
@@ -91,12 +299,13 @@ def load_modes(mode_path):
     VH = np.array(f['VH'])
     return U, S, VH
 
+
 def load_cage_data(cage_path):
     data = np.loadtxt(cage_path, skiprows=1)
     cage_slice = np.zeros_like(data)
     cage_slice[:, 0] = data[:, 0]
     cage_slice[:, 1] = data[:, 2]
-    cage_slice[:, 1] = cage_slice[:, 1]
+    cage_slice[:, 2] = 0
 
     return cage_slice
 
@@ -157,8 +366,6 @@ def plot_10_modes_cage(foil, U, rbf, cage_slice, n=10):
         ax.set_xlabel('x/c', fontsize=12)
         ax.set_ylabel('z', fontsize=12)
 
-        
-
         ax.set_title('Mode {}'.format(i), fontsize=18)
 
     fig.tight_layout()
@@ -177,45 +384,85 @@ def add_z(points):
 
     return points_3D
 
+# Load data
 
 data_dir = '/home/tom/Documents/University/Coding/zCFD_Utils/data/UIUC Aerofoil Library/Smoothed/'
 save_loc = '/home/tom/Documents/University/Coding/zCFD_Utils/data/UIUC Aerofoil Library/Modal_data.h5'
 
-# generate_modes(data_dir=data_dir, save_loc=save_loc)
 U, S, VH = load_modes('/home/tom/Documents/University/Coding/zCFD_Utils/data/UIUC Aerofoil Library/Modal_data.h5')
 
 cage_slice = load_cage_data('/home/tom/Documents/University/Coding/zCFD_Utils/data/domain_ordered.ctr.asa.16')
-cage_slice[:, 2] = cage_slice[:, 2] * 3
 
 naca0012 = add_z(np.loadtxt('/home/tom/Documents/University/Coding/zCFD_Utils/data/UIUC Aerofoil Library/Smoothed/NACA 0012-64.dat', skiprows=3))
+SNL_FFA = add_z(np.loadtxt('/home/tom/Documents/University/Coding/zCFD_Utils/data/foils/SNL-FFA-W3-500.dat', skiprows=3))
 
+blade = Blade_geom('/home/tom/Documents/University/Coding/zCFD_Utils/data/IEA-15-240-RWT_FineGrid.yaml')
+
+blade_surf = Blade_surf('/home/tom/Documents/University/Coding/zCFD_Utils/data/IEA_15MW_New_rot.srf.plt')
+
+cage = control_cage(blade, 10, cage_slice)
+
+# Create rotation matrix
+theta = np.deg2rad(-10)
+rotation_matrix = np.array([[np.cos(theta), -np.sin(theta), 0], [np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
+SNL_FFA = SNL_FFA @ rotation_matrix
+cage_rot = cage_slice @ rotation_matrix
 # Couple control cage with naca0012 profile
 
 rbf = py_rbf.UoB_coupling(cage_slice, naca0012)
 rbf.generate_transfer_matrix(0.5, 'c2', False)
 
-# Plot default aerofoil
-plt.plot(naca0012[:, 0], naca0012[:, 1])
+# Couple aerodynamic surface to control cage
+rbf_ca = py_rbf.UoB_coupling(SNL_FFA, cage_slice @ rotation_matrix)
+rbf_ca.generate_transfer_matrix(0.5, 'c2', False)
 
-# perturb naca0012 by first mode, and plot result
+rbf_cs = py_rbf.UoB_coupling(blade_surf.points, cage.dump_points())
+rbf_cs.generate_transfer_matrix(10, 'c2', False)
 
-# deformed_aerofoil = deform_aerofoil(naca0012, U, 0, weight=0.5)
-deformed_aerofoil = naca0012.copy()
+
+# perturb naca0012 by first mode
+
+deformed_aerofoil = deform_aerofoil(naca0012, U, 0, weight=0.5)
 deformations = add_z(deformed_aerofoil - naca0012)
-deformations[235:255, 1] = 0.05
-deformations[45:65, 1] = 0.05
-# deformations = np.zeros_like(deformations)
-# deformations[:, 1] = 1
 
+# Interpolate displacements to control cage
+
+# issue line- check out interp in py_rbf
+a = rbf.H @ deformations.copy()
+a_rot = a @ rotation_matrix
+
+# Create rbf system between cage and surface
+rbf_ca = py_rbf.UoB_coupling(SNL_FFA, cage_slice)
+rbf_ca.generate_transfer_matrix(0.5, 'c2', False)
+
+plt.plot(naca0012[:, 0], naca0012[:, 1])
 plt.plot(naca0012[:, 0] + deformations[:, 0], naca0012[:, 1] + deformations[:, 1])
+plt.plot(SNL_FFA[:, 0], SNL_FFA[:, 1])
+plt.plot(cage_rot[:, 0], cage_rot[:, 1], 'ro-')
+plt.plot(cage_rot[:, 0] + a_rot[:, 0], cage_rot[:, 1] + a_rot[:, 1], 'bo-')
 
-# Interpolate displacements
+# interpolate deformations
+surf_def = rbf_ca.H @ a
+plt.plot(SNL_FFA[:, 0] + surf_def[:, 0], SNL_FFA[:, 1] + surf_def[:, 1])
 
-a = rbf.H @ deformations.copy() # issue line- check out interp in py_rbf
+plt.legend(['Naca0102', 'Naca0012 Mode0', 'Control Cage', 'Control Cage Mode0', 'SNL-FFA-W3-500', 'SNL-FFA-W3-500 Mode0'])
+plt.show()
 
-plt.plot(cage_slice[:, 0], cage_slice[:, 1], 'ro-')
-plt.plot(cage_slice[:, 0] + a[:, 0], cage_slice[:, 1] + a[:, 1], 'bo-')
 
-plot_10_modes_cage(naca0012, U, rbf, cage_slice, n=20)
+for i in range(cage.n_sections):
+    deformed_aerofoil = deform_aerofoil(naca0012, U, 8, weight=1)
+    deformations = add_z(deformed_aerofoil - naca0012)
 
-# Cast control cage along blade geometry
+    a = rbf.H @ deformations.copy()
+
+    cage.convert_section_deformations(a, i)
+
+
+surf_def = rbf_cs.H @ cage.dump_deformations()
+
+# Write out deformations
+blade_surf.write_deformed('/home/tom/Documents/University/Coding/zCFD_Utils/data/IEA_15MW_New_rot_deformed.srf.plt', surf_def)
+cage.write_cage('/home/tom/Documents/University/Coding/zCFD_Utils/data/cage.plt')
+cage.write_deformed('../data/deformed_cage.plt')
+
+
